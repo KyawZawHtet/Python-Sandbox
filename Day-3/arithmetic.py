23d694cd673d92b51dc7ee0b65d9ac9773267c70
KyawZawHtet <kyawzawhtet2268@gmail.com>
2025-12-12 15:47:04 +0630

    Update content

---
+++ Day-3/arithmetic.py
@@ -0,0 +1,13 @@
+a = 10
+b = 3
+print("Sum is", a + b, type(a + b))
+print("Subtract is", a - b, type(a - b))
+print("Multiply is", a * b, type(a * b))
+
+# Note: Division always results in a float
+print("Divide is", a / b, type(a / b))
+
+# Integer Division
+print("Integer Division is", a // b, type(a // b))
+print("Power is", a ** b, type(a ** b))
+print("Modulus is", a % b, type(a % b))
---
+++ Day-3/order.py
@@ -0,0 +1,9 @@
+a = 2
+b = 3
+c = 4
+
+print("a + b * c =", a + b * c, type(a + b * c))
+print("a + b * c =", (a + b**2) * c, type(a + b * c))
+print("a + b * c =", (a + b**2 * c, type(a + b * c)))
+print("a + b * c =", (a + b**+2 * c, type(a + b * c)))
+print("a + -b", (a + -b, type(a + -b)))
